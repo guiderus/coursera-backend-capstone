88e1b3db2a7d4312c0e0856e05eadd3646898c15
guiderus <guiderus@gmail.com>
2023-08-17 01:18:55 +0700

    Protect /api/menu-items by Auth Token

--- littlelemon/LittleLemonAPI/urls.py
+++ littlelemon/LittleLemonAPI/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
-from .views import MenuItemView, SingleMenuItemView
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu-items', MenuItemView.as_view()),
-    path('menu-items/<int:pk>', SingleMenuItemView.as_view())
+    path('menu-items', views.MenuItemView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('message', views.msg, name='msg'),
+    path('api-token-auth', obtain_auth_token)
 ]
